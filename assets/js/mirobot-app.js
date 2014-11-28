389163697d11ab6674eb79bd70f68e358dceb5aa
Ben Pirt <ben@pirt.co.uk>
2014-11-28 14:40:12 +0000

    Improve the connection handling

--- assets/js/mirobot-app.js
+++ assets/js/mirobot-app.js
@@ -1,9 +1,10 @@
 MirobotApp = function(ready){
   this.ready = ready;
+  this.has_connected = false;
   this.init();
 }
 
-MirobotApp.prototype.init = function(){
+MirobotApp.prototype.extractConfig = function(){
   var self = this;
   self.hashConfig = {};
   if(window.location.hash !== ''){
@@ -12,19 +13,74 @@ MirobotApp.prototype.init = function(){
       self.hashConfig[split[0]] = split[1];
     });
   }
-  self.connState = document.querySelector('#header .connState');
-  var host = self.hashConfig['m'] || window.location.hostname;
-  self.mirobot = new Mirobot('ws://' + host + ':8899/websocket');
-  self.mirobot.addListener(function(r){ self.handler(r) });
+}
+
+MirobotApp.prototype.init = function(){
+  this.initted = false;
+  this.connect();
+}
+
+MirobotApp.prototype.connect = function(){
+  var self = this;
+  self.extractConfig();
+  self.connState = self.hashConfig['m'] ? 'connecting' : 'not_set';
+  if(self.hashConfig['m']){
+    self.mirobot = new Mirobot('ws://' + self.hashConfig['m'] + ':8899/websocket');
+    self.mirobot.addListener(function(r){ self.handler(r) });
+  }
+  self.setConnState();
 }
 
 MirobotApp.prototype.handler = function(state){
   if(state === 'connected'){
-    this.connState.innerHTML = '&#10003; Connected to Mirobot';
-    this.connState.className = 'connState connected';
-    this.ready(this.mirobot);
+    this.connState = 'connected';
+    this.has_connected = true;
+    if(!this.initted){
+      this.initted = true;
+      this.ready(this.mirobot);
+    }
   }else if(state === 'disconnected'){
-    this.connState.innerHTML = '&#10007; Reconnecting to Mirobot';
-    this.connState.className = 'connState disconnected';
+    if(!this.has_connected){
+      this.connState = 'cant_connect';
+    }else{
+      this.connState = 'disconnected';
+    }
+  }
+  this.setConnState();
+}
+
+MirobotApp.prototype.configure = function(e){
+  var ip = prompt("Enter the address for your Mirobot here:\n (e.g. 192.168.1.100)", this.hashConfig['m']);
+  if(ip){
+    window.location = '#m=' + ip;
+    this.connect();
+  }
+  e.preventDefault();
+  return false;
+}
+
+MirobotApp.prototype.setConnState = function(){
+  var self = this;
+  var cs = document.querySelector('#header .connState');
+  switch(this.connState){
+    case 'not_set':
+      cs.innerHTML = '&#10007; <a href="#">Configure Mirobot connection</a>';
+      cs.querySelector('a').addEventListener('click', function(e){ self.configure(e) });
+      cs.className = 'connState';
+      break;
+    case 'connected':
+      cs.innerHTML = '&#10003; Connected to Mirobot';
+      cs.className = 'connState connected';
+      break;
+    case 'cant_connect':
+      cs.innerHTML = '&#10007; <a href="#">Can\'t connect to Mirobot<a>';
+      cs.querySelector('a').addEventListener('click', function(e){ self.configure(e) });
+      cs.className = 'connState error';
+      break;
+    case 'disconnected':
+      cs.innerHTML = '&#10007; <a href="#">Reconnecting to Mirobot</a>';
+      cs.querySelector('a').addEventListener('click', function(e){ self.configure(e) });
+      cs.className = 'connState error';
+      break;
   }
 }
