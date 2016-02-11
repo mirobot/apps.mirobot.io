4433ad7dac2bda0c99ac5112790debf454e1596e
Ben Pirt <ben@pirt.co.uk>
2016-02-11 09:53:18 +0000

    Improve the links function

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,10 +1,16 @@
 var updateLinks = function(){
-	[].forEach.call(document.links, function(l) {
-		if(l.href.slice(-1) === '/'){
-			l.href = l.href + 'index.html';
-		}
-		l.href = l.href + document.location.hash;
-	});
+  [].forEach.call(document.links, function(l) {
+    // Make the URL have index.html if running as a chrome app
+    if('chrome' in window && 'storage' in window.chrome){
+      if(l.href.slice(-1) === '/'){
+        l.href += 'index.html';
+      }
+    }
+    // Add the query for the language
+    l.href += document.location.search;
+    // Add the hash for configuring Mirobot    
+    l.href += document.location.hash;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', updateLinks);
