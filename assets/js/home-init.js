e9f8318f9b91c3ea89a55bf297e5a0f21cda40be
Ben Pirt <ben@pirt.co.uk>
2016-02-18 15:21:51 +0000

    Hide the chrome app when running as a Chrome App

--- assets/js/home-init.js
+++ assets/js/home-init.js
@@ -1,1 +1,10 @@
 l10nMenu('l10n', baseLanguages);
+
+if('chrome' in window && 'storage' in window.chrome){
+  document.addEventListener("DOMContentLoaded", function(){
+    var chr = document.querySelectorAll('.chromehide');
+    for (var i=0; i<chr.length; i++){
+      chr[i].classList.add('hidden');
+    }
+  });
+}
