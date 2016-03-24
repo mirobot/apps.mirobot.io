ce3c733ead9629d3575ffc170a552a97d8613f5c
Ben Pirt <ben@pirt.co.uk>
2016-03-24 09:16:28 +0000

    Update settings for Chrome app

--- chrome.js
+++ chrome.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('chrome.html', {
     'outerBounds': {
-      'width': 850,
+      'width': 1120,
       'height': 700
     }
   });
