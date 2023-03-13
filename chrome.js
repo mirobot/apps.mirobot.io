bdd644b7109897b1039db96878c6b9dd7aec439c
Ben Pirt <ben@pirt.co.uk>
2023-03-13 06:58:12 +0000

    Remove Chrome app assets and the Scratch app since Flash is no longer usable

--- chrome.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('chrome.html', {
-    'outerBounds': {
-      'width': 1120,
-      'height': 700
-    }
-  });
-});
